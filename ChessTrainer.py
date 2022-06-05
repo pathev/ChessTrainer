f3a79e805879f7348869cf1cd8b5e81b3b201a39
pathev <50081414+pathev@users.noreply.github.com>
2022-06-05 07:55:05 +0200

    arrow square

--- ChessTrainer.py
+++ ChessTrainer.py
@@ -679,6 +679,7 @@ class GUI(tk.Tk):
         self.selected_from_square = chess.square(current_column, current_row)
         self.canvas.bind("<Motion>",lambda e:self.arrow_creating(e,color))
         self.canvas.bind("<ButtonRelease>",lambda e:self.arrow_end(e,color))
+        self.arrow_creating(event,color)
 
     def arrow_creating(self, event,color):
 
@@ -694,6 +695,9 @@ class GUI(tk.Tk):
 
         if self.selected_from_square != square:
             self.draw_arrow(self.selected_from_square,square,comment_arrow_color.get(color))
+        else:
+            self.draw_arrow_square(self.selected_from_square,comment_arrow_color.get(color))
+
 
     def arrow_end(self, event,color):
         self.canvas.unbind("<Motion>")
@@ -707,21 +711,20 @@ class GUI(tk.Tk):
         
         square = chess.square(current_column, current_row)
 
-        if self.selected_from_square != square:
-            arrows = self.pgn.arrows()
-            found = False
-            for arrow in arrows:
-                from_square = arrow.tail
-                to_square = arrow.head
-                if (from_square,to_square) == (self.selected_from_square,square):
-                    found = True
-                    arrows.remove(arrow)
-                    if arrow.color != color:
-                        arrows.append(chess.svg.Arrow(tail=self.selected_from_square,head=square,color=color))
-                    break
-            if not found:
-                arrows.append(chess.svg.Arrow(tail=self.selected_from_square,head=square,color=color))
-            self.pgn.set_arrows(arrows)
+        arrows = self.pgn.arrows()
+        found = False
+        for arrow in arrows:
+            from_square = arrow.tail
+            to_square = arrow.head
+            if (from_square,to_square) == (self.selected_from_square,square):
+                found = True
+                arrows.remove(arrow)
+                if arrow.color != color:
+                    arrows.append(chess.svg.Arrow(tail=self.selected_from_square,head=square,color=color))
+                break
+        if not found:
+            arrows.append(chess.svg.Arrow(tail=self.selected_from_square,head=square,color=color))
+        self.pgn.set_arrows(arrows)
             
         self.refresh()
 
@@ -808,7 +811,10 @@ class GUI(tk.Tk):
             from_square = arrow.tail
             to_square = arrow.head
             color = arrow.color
-            self.draw_arrow(from_square,to_square,comment_arrow_color.get(color))
+            if from_square == to_square:
+                self.draw_arrow_square(from_square,comment_arrow_color.get(color))
+            else:
+                self.draw_arrow(from_square,to_square,comment_arrow_color.get(color))
 
     def draw_variations_arrows(self):
         for i,node in enumerate(self.pgn.variations):
@@ -817,7 +823,19 @@ class GUI(tk.Tk):
             to_square = m.to_square
             self.draw_arrow(from_square,to_square,arrow_color[i%len(arrow_color)])
 
-    def draw_arrow(self,from_square,to_square,color,analyze=False,outline=False):
+    def draw_arrow_square(self,square,color):
+        row1,col1=chess.square_rank(square),chess.square_file(square)
+        if self.flipped:
+            x1 = ((7-col1) * self.square_size)
+            y1 = ((row1) * self.square_size)
+        else:
+            x1 = ((col1) * self.square_size)
+            y1 = ((7-row1) * self.square_size)
+        l=int(self.square_size*0.075)
+        e=1+l//2
+        self.canvas.create_rectangle(x1+e,y1+e,x1+self.square_size-e,y1+self.square_size-e,width=l,outline=color,tags="arrow")
+
+    def draw_arrow(self,from_square,to_square,color,analyze=False):
         row1,col1=chess.square_rank(from_square),chess.square_file(from_square)
         row2,col2=chess.square_rank(to_square),chess.square_file(to_square)
         if self.flipped:
@@ -838,10 +856,8 @@ class GUI(tk.Tk):
             y2-=self.square_size//8
         elif y2<y1:
             y2+=self.square_size//8
-        if color is None:
-            color = "#AAAAAA"
         tags = "analyze_arrow" if analyze else "arrow"
-        return (from_square,to_square),self.canvas.create_line(x1,y1,x2,y2,arrow=tk.LAST,width=self.square_size//5,arrowshape=(15,12,5),fill=color,tags=tags)
+        self.canvas.create_line(x1,y1,x2,y2,arrow=tk.LAST,width=self.square_size//5,arrowshape=(15,12,5),fill=color,tags=tags)
 
     def redraw_pieces(self):
         self.icons={}
@@ -868,7 +884,6 @@ class GUI(tk.Tk):
             xsize = (event.width-1) // 8
             ysize = (event.height-1) // 8
             self.square_size = min(xsize, ysize)
-
         try:
             cur_move=self.chessboard.peek()
             from_square=cur_move.from_square
